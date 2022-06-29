951eaad18c84eed93c111e5ac780a825a188b96e
Avery Starling <merestarling@gmail.com>
2022-06-29 12:53:46 -0700

    dojoweather

---
+++ webfund/june29/dojoweather/script.js
@@ -0,0 +1,14 @@
+function weatherLoading(){
+    alert("Loading weather report...");
+}
+
+
+function changeUnits(){
+}
+
+
+//accept cookies
+var cookiesAlert = document.querySelector("#cookies");
+function cookiesAccepted(){
+    cookiesAlert.remove();
+}
