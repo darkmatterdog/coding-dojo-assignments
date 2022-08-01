f5b8897943b540e5d3fe1315a3effc5d5de36c5c
Avery Starling <merestarling@gmail.com>
2022-08-01 08:33:35 -0700

    java intro

---
+++ java/fundamentals/first_java_program/First.java
@@ -0,0 +1,11 @@
+public class First {
+    public static void main(String[] args){
+        String name = "Avery Starling";
+        int age = 21;
+        String hometown = "Seattle, WA";
+
+        System.out.println("My name is "+name);
+        System.out.println("I am "+age+" years old");
+        System.out.println("My hometown is "+hometown);
+    }
+}
